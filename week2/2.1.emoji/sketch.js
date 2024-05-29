17879c055c1ea3230c4cf5b778eb6b131589760b
Darryl6767 <94204001+Darryl6767@users.noreply.github.com>
2024-05-28 22:42:24 -0400

    🙄🙄🙄🙄🙄

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -3,5 +3,45 @@ function setup() {
 }
 
 function draw() {
+  
+  //body of the emoji
   background(220);
+  
+  fill("gold");
+
+ stroke("black")
+  strokeWeight(10)
+  
+  circle(200,200,250);
+  
+  
+  //eyes 
+  
+  
+  fill("white")
+  stroke("white")
+  strokeWeight("20")
+  ellipse(150,180,45,45)
+  
+   fill("white")
+  stroke("white")
+  strokeWeight("20")
+  ellipse(250,180,45,45)
+  
+  fill("black")
+  stroke("black") 
+  ellipse(150,160,1,10)
+  
+  
+   
+  fill("black")
+  stroke("black") 
+  ellipse(250,160,1,10)
+  
+  
+  //mouth
+  
+  stroke("black")
+  strokeWeight("10")
+  line(220,300,180,300)
 }
