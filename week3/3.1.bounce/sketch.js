9c6e2c4ca4850002dd803f375848dc984112fb89
Darryl6767 <94204001+Darryl6767@users.noreply.github.com>
2024-06-07 13:50:15 -0400

    Update sketch.js

--- week3/3.1.bounce/sketch.js
+++ week3/3.1.bounce/sketch.js
@@ -1,7 +1,42 @@
+let x = 0;
+
+let y = 0;
+
+let xspeed = 5;
+
+let yspeed = 5;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 300);
+  fill("black");
+
 }
 
 function draw() {
-  background(220);
+background(220);
+  
+translate(width / 2, height / 2);
+
+ellipse(x,y,50);
+
+  
+
+  if(x > 175 | x < -175){
+    
+    xspeed = -xspeed;
+    
+    fill(random(0),100,100);
+ }
+
+  if(y > 125 | y < -125){
+    
+    yspeed = -yspeed;
+    
+    fill(random(100),100,100);
+ }
+  y+= yspeed;
+  
+  x+= xspeed; 
+  
+ 
 }
