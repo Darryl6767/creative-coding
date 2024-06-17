7b21be89b4222ff37152cb19e6837e2de9899ecd
Darryl6767 <94204001+Darryl6767@users.noreply.github.com>
2024-06-17 12:26:07 -0400

    Update sketch.js

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,6 +1,6 @@
 let img;
-let sampleX =10;
-let sampleY =10;
+let sampleX =45;
+let sampleY= 45;
 
 function preload(){
   img= loadImage( 'https://darryl6767.github.io/creative-coding/week2/pixels/iceman.png');
@@ -19,13 +19,15 @@ function draw() {
   for(let x=0; x <= img.width; x += sampleX)
     {
       for(let y=0; y<= img.height; y += sampleY){
-        var index =(x*y* img.width)*4;
-	      
+        var index =(x + y* img.width)*4;
+        
         var r= img.pixels[index + 0];
-	      
+        
          var g= img.pixels[index + 1];
-	      
+        
          var b= img.pixels[index + 2];
+
+         
         
         
         fill(r,g,b);
