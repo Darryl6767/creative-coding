00dde7dd843c9849fc7610d723b0b9c8add57283
Darryl6767 <94204001+Darryl6767@users.noreply.github.com>
2024-06-23 11:26:21 -0400

    Update sketch.js

--- week3/3.2.follower/sketch.js
+++ week3/3.2.follower/sketch.js
@@ -1,75 +1,75 @@
-let debug=true;
+let debug = false;
 
-let x=0;
-
-let y=0;
-
-let d=0;
-
-let speedfactor =3;
-
+let x = 0;
+let y = 0;
+let d = 0;
+let speedfactor = 3;
 let speedx = speedfactor;
-
-let speedy= speedfactor;
-
+let speedy = speedfactor;
 let score = 0;
+let img; // Store the baseball sprite
+
+function preload() {
+  img = loadImage('https://darryl6767.github.io/creative-coding/week3/3.2.follower/baseball.png');
+}
+
 function setup() {
   createCanvas(800, 800);
-  
-  x=random(0,width);
-  y=random(0,height);
-  
+  x = random(0, width);
+  y = random(0, height);
 }
 
 function draw() {
   background(220);
-  
-  
-  //Function that represents the distance formula
-  d = sqrt((x-mouseX)**2 +(y-mouseY)**2);
-  
-  //If statement for collision
-  
-  if(d<25){
+
+  // This is the distance formula
+  d = sqrt((x - mouseX) ** 2 + (y - mouseY) ** 2);
+
+  // Collision
+  if (d < 25) {
     score -= 1;
-    x=random(0,width);
-  y=random(0,height);
+    x = random(0, width);
+    y = random(0, height);
   }
-  
-  //movement
+
+  // Movement
   x += speedx;
-  y+= speedy;
-  
-  //Circle for the follower
-  
-  circle(x,y,50);
-  
-  if(mouseX > x){
-    //right
-    speedx=speedfactor;
-  }else{
-    //left
-     speedx=-speedfactor;
+  y += speedy;
+
+  // To make sure the image loads in
+  if (img) {
+    // Baseball sprite image
+    image(img, x - 25, y - 25, 50, 50); // Centering the image(x, y)
   }
-  
-    if(mouseY > y){
-    //right
-    speedy=speedfactor;
-  }else{
-    //left
-     speedy=-speedfactor;
+
+  if (mouseX > x) {
+    // Right
+    speedx = speedfactor;
+  } else {
+    // Left
+    speedx = -speedfactor;
   }
-  
-  if(debug){
-    
-  
+
+  if (mouseY > y) {
+    // Down
+    speedy = speedfactor;
+  } else {
+    // Up
+    speedy = -speedfactor;
+  }
+
+  // Display the score
   textSize(20);
-  text("mouse in X direction:" + mouseX, 50,50);
-    text("mouse in Y direction:" + mouseY, 50,70);
-    text("x:" + x,50,120);
-     text("y:" + y,50,140);
-       text("d:" + d,50,160);
-     text("score:" + score,50,180);
-    
-    }
+  fill(0);
+  text("Score: " + score, 10, 30);
+
+  if (debug) {
+    textSize(20);
+    text("mouse in X direction:" + mouseX, 50, 50);
+    text("mouse in Y direction:" + mouseY, 50, 70);
+    text("x:" + x, 50, 120);
+    text("y:" + y, 50, 140);
+    text("d:" + d, 50, 160);
+    text("score:" + score, 50, 180);
+  }
 }
